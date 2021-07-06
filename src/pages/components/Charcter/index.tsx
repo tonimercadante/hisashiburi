bfba2d7f9c65900c2a56e68160bd404d7ea1ac8b
Toni Mercadante <mercadanteneto@gmail.com>
2021-07-06 08:43:27 -0300

    a

---
+++ src/pages/components/Charcter/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function Character() {
+  return (
+    <div>
+      <h1>Character</h1>
+    </div>
+  );
+}
--- src/pages/components/Search/index.tsx
+++ src/pages/components/Search/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./search.module.scss";
+import styles from "./styles.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
