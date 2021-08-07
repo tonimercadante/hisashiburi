cc4a3f7c0644cb9a7526d59a6222dc687c803ff6
Toni Mercadante <mercadanteneto@gmail.com>
2021-08-07 03:42:16 -0300

    modal scroll

--- src/pages/components/NavBar/index.tsx
+++ src/pages/components/NavBar/index.tsx
@@ -6,8 +6,12 @@ export default function NavBar() {
         <nav className={styles.navBar}>
             <p>Hisashiburi</p>
             <ul>
+                <li><Link href="/"><a>Home</a></Link></li>
+                <li>/</li>
                 <li><Link href="/api-info"><a>Api</a></Link></li>
+                <li>/</li>
                 <li><Link href="/bot"><a>Bot</a></Link></li>
+                <li>/</li>
                 <li><Link href="/about"><a>About</a></Link></li>
             </ul>
         </nav>
